1fd92b0f3c2524588a7dff45ca5d492bb5f8d00d
Jack Jansen <Jack.Jansen@cwi.nl>
2016-10-05 23:19:18 +0200

    Renaming

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -f WiFi-Witty-Gizwits-ESP-12F.fzpz
-rm -f dist/WiFi-Witty-Gizwits-ESP-12F.fzpz
-zip -j dist/WiFi-Witty-Gizwits-ESP-12F.fzpz src/*.fzp
-zip -j dist/WiFi-Witty-Gizwits-ESP-12F.fzpz src/*.svg
+rm -f Bare-ESP-12F.fzpz
+rm -f dist/Bare-ESP-12F.fzpz
+zip -j dist/Bare-ESP-12F.fzpz src/*.fzp
+zip -j dist/Bare-ESP-12F.fzpz src/*.svg
